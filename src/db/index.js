e14e587e3cd0a0e1a4d8fd7584f7d35e2015b38c
Usman Rizwan <81845304+usman-rizwan@users.noreply.github.com>
2024-02-04 15:01:39 +0500

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth , createUserWithEmailAndPassword ,signInWithEmailAndPassword , onAuthStateChanged ,signOut } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAktS2Z1YW19zC6DzgaulzvJBC-lIKfdqo",
-    authDomain: "login-signup-form-d1674.firebaseapp.com",
-    databaseURL: "https://login-signup-form-d1674-default-rtdb.firebaseio.com",
-    projectId: "login-signup-form-d1674",
-    storageBucket: "login-signup-form-d1674.appspot.com",
-    messagingSenderId: "487043740599",
-    appId: "1:487043740599:web:770a92712a30b59c81ae32"
+
   };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
