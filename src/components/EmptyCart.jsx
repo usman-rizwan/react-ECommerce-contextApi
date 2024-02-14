56cc799b72f5ca0d781679b792f8e648c8eadc26
usman_rizwan <usmanrizwan771@gmail.com>
2024-02-14 13:46:30 +0500

    Update EmptyCart.jsx

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import {  Empty } from "antd";
+import { Button } from "@nextui-org/react";
+import { Link } from "react-router-dom";
 
-const EmptyCart = ({ description ,class_name }) => <Empty description={description}  className={class_name}/>;
+const EmptyCart = ({ description ,class_name }) =>{
+    return(
+        <div>
+<Empty description={description}  className={class_name}/>
+<div className="flex justify-center items-center mt-5">
+
+<Button   variant="flat"
+        color="primary"> <Link to={'/'}>Back Home</Link> </Button>
+</div>
+        </div>
+    )
+} 
+    
 export default EmptyCart;
