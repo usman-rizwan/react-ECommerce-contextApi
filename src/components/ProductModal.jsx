58abb3787f3f1ced74169c8d8d64f4c1208669c2
usman_rizwan <usmanrizwan771@gmail.com>
2024-03-19 16:44:59 +0500

    Update ProductModal.jsx

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback  ,useContext, useEffect} from "react";
+import React, { useState, useCallback, useContext, useEffect } from "react";
 import {
   Modal,
   ModalContent,
@@ -14,24 +14,20 @@ import {
   SwapOutlined,
   ZoomInOutlined,
   ZoomOutOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 import Cart from "../context/cart";
 import axios from "axios";
-import {Spinner , Chip} from "@nextui-org/react";
-import ImageLoading from '../assets/loading.gif'
-import { Image, Space ,notification } from 'antd';
-
+import { Spinner, Chip } from "@nextui-org/react";
+import ImageLoading from "../assets/loading.gif";
+import { Image, Space, notification } from "antd";
 
 export default function ProductModal({ id }) {
-  const {cart , setCart} = useContext(Cart)
-  const { isOpen, onOpen, onOpenChange ,onClose} = useDisclosure();
+  const { cart, setCart } = useContext(Cart);
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
   const [backdrop, setBackdrop] = useState(null);
   const [productDetails, setProductDetails] = useState({});
   const [loading, setloading] = useState(true);
   const [scrollBehavior, setScrollBehavior] = useState("inside");
-
-
- 
 
   const handleOpen = (backdrop) => {
     setBackdrop(backdrop);
@@ -45,37 +41,38 @@ export default function ProductModal({ id }) {
         setloading(false);
       })
       .catch((err) => console.log(err));
-  }, []);  
+  }, [id]);
   useEffect(() => {
     fetchData();
   }, [id]);
   const addToCartHandler = (data) => {
     console.log(data);
-  
+
     // Add quantity field to the API data
     const dataWithQty = { ...data, qty: 1 };
     const storedCartData = localStorage.getItem("cart");
     let cartData = storedCartData ? JSON.parse(storedCartData) : [];
-    const existingItemIndex = cartData.findIndex(item => item.id === dataWithQty.id);
+    const existingItemIndex = cartData.findIndex(
+      (item) => item.id === dataWithQty.id
+    );
     if (existingItemIndex !== -1) {
       cartData[existingItemIndex].qty += 1;
     } else {
       cartData.push(dataWithQty);
     }
-  
+
     localStorage.setItem("cart", JSON.stringify(cartData));
     setCart(cartData);
     console.log("Cart Data:", cartData);
-  
+
     notification.success({
-      message: 'Item Added Successfully',
+      message: "Item Added Successfully",
       description: `${dataWithQty.title}!`,
-      duration: 1.5
+      duration: 1.5,
     });
-  
+
     onClose();
   };
-
 
   return (
     <div className="outfit">
@@ -91,42 +88,95 @@ export default function ProductModal({ id }) {
           View Details
         </Button>
       </div>
-      <Modal backdrop={backdrop} isOpen={isOpen} onClose={onClose} onOpenChange={onOpenChange} isDismissable={false} scrollBehavior={scrollBehavior}>
+      <Modal
+        backdrop={backdrop}
+        isOpen={isOpen}
+        onClose={onClose}
+        onOpenChange={onOpenChange}
+        isDismissable={false}
+        scrollBehavior={scrollBehavior}
+      >
         <ModalContent className="md:h-[60vh]">
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">
-              {loading ? <img  className="w-50 h-40 object-contain rounded-lg"  src={ImageLoading} />  :  <div className=" flex justify-center items-center"><Image
-      width={200}
-      src={productDetails.image}
-      preview={{
-        toolbarRender: (
-          _,
-          {
-            transform: { scale },
-            actions: { onFlipY, onFlipX, onRotateLeft, onRotateRight, onZoomOut, onZoomIn },
-          },
-        ) => (
-          <Space size={12} className="toolbar-wrapper">
-            <SwapOutlined rotate={90} onClick={onFlipY} />
-            <SwapOutlined onClick={onFlipX} />
-            <RotateLeftOutlined onClick={onRotateLeft} />
-            <RotateRightOutlined onClick={onRotateRight} />
-            <ZoomOutOutlined disabled={scale === 1} onClick={onZoomOut} />
-            <ZoomInOutlined disabled={scale === 50} onClick={onZoomIn} />
-          </Space>
-        ),
-      }}
-    /></div>}
-                 {loading ?   <Spinner color="primary" className="mt-10 "/> : <div className="capitalize outfit">{productDetails.title}</div> }
+                {loading ? (
+                  <img
+                    className="w-50 h-40 object-contain rounded-lg"
+                    src={ImageLoading}
+                  />
+                ) : (
+                  <div className=" flex justify-center items-center">
+                    <Image
+                      width={200}
+                      src={productDetails.image}
+                      preview={{
+                        toolbarRender: (
+                          _,
+                          {
+                            transform: { scale },
+                            actions: {
+                              onFlipY,
+                              onFlipX,
+                              onRotateLeft,
+                              onRotateRight,
+                              onZoomOut,
+                              onZoomIn,
+                            },
+                          }
+                        ) => (
+                          <Space size={12} className="toolbar-wrapper">
+                            <SwapOutlined rotate={90} onClick={onFlipY} />
+                            <SwapOutlined onClick={onFlipX} />
+                            <RotateLeftOutlined onClick={onRotateLeft} />
+                            <RotateRightOutlined onClick={onRotateRight} />
+                            <ZoomOutOutlined
+                              disabled={scale === 1}
+                              onClick={onZoomOut}
+                            />
+                            <ZoomInOutlined
+                              disabled={scale === 50}
+                              onClick={onZoomIn}
+                            />
+                          </Space>
+                        ),
+                      }}
+                    />
+                  </div>
+                )}
+                {loading ? (
+                  <Spinner color="primary" className="mt-10 " />
+                ) : (
+                  <div className="capitalize outfit">
+                    {productDetails.title}
+                  </div>
+                )}
               </ModalHeader>
               <ModalBody className="capitalize outfit">
-              {productDetails.description}<br/>
-              <div>
-              {loading ? "" : <div >  <Chip color="warning" variant="dot" className="bg-[#27272a] text-white ">{productDetails.category}</Chip><br/><p className="mt-5">Price : <b> ${productDetails.price}/-</b></p> </div>   }
-              </div>
+                {productDetails.description}
+                <br />
+                <div>
+                  {loading ? (
+                    ""
+                  ) : (
+                    <div>
+                      {" "}
+                      <Chip
+                        color="warning"
+                        variant="dot"
+                        className="bg-[#27272a] text-white "
+                      >
+                        {productDetails.category}
+                      </Chip>
+                      <br />
+                      <p className="mt-5">
+                        Price : <b> ${productDetails.price}/-</b>
+                      </p>{" "}
+                    </div>
+                  )}
+                </div>
               </ModalBody>
-              <ModalFooter >
+              <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
                   Close
                 </Button>
@@ -137,7 +187,6 @@ export default function ProductModal({ id }) {
                 >
                   Add To Cart
                 </Button>
-               
               </ModalFooter>
             </>
           )}
